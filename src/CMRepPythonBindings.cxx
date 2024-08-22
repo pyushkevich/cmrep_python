8c5840af767e8c72409b337cc7d3294bed380701
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-21 21:32:54 -0400

    missing header

--- src/CMRepPythonBindings.cxx
+++ src/CMRepPythonBindings.cxx
@@ -24,6 +24,7 @@
 =========================================================================*/
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
+#include <sstream>
 namespace py=pybind11;
 
 using namespace std;
